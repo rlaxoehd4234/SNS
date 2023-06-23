94410312b2145ff4b0cea37a12c08fca5a3da04b
kimtaedong <rlaxoehd4234@naver.com>
2023-06-23 10:56:14 +0900

    [FEAT,REFACTOR] Spring Doc openApi 추가 ,  및 컨트롤러 @Operation summary 추가

--- FFSNS/src/main/java/com/ffsns/sns/conguration/SwaggerConfig.java
+++ FFSNS/src/main/java/com/ffsns/sns/conguration/SwaggerConfig.java
@@ -1,2 +1,27 @@
-package com.ffsns.sns.conguration;public class SwaggerConfig {
+package com.ffsns.sns.conguration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "FFSNS 명세서",
+        description = "외국인 친구와의 화합을 위한 FFSNS API 명세서",
+        version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi snsOpenApi(){
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+                .group("SNS API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
 }
--- FFSNS/src/main/java/com/ffsns/sns/controller/PostController.java
+++ FFSNS/src/main/java/com/ffsns/sns/controller/PostController.java
@@ -8,6 +8,7 @@ import com.ffsns.sns.controller.response.PostResponse;
 import com.ffsns.sns.controller.response.Response;
 import com.ffsns.sns.model.Post;
 import com.ffsns.sns.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +23,7 @@ public class PostController {
 
     private final PostService postService;
 
+    @Operation(summary = "게시글 생성")
     @PostMapping
     public Response<Void> create (@RequestBody PostCreateRequest request, Authentication authentication){
 
@@ -29,12 +31,14 @@ public class PostController {
         return Response.success();
     }
 
+    @Operation(summary = "게시글 수정")
     @PutMapping("/{postId}")
     public Response<PostResponse> modify( @PathVariable Integer postId, @RequestBody PostModifyRequest request, Authentication authentication){
         Post post = postService.modify(request.getTitle(), request.getBody(), authentication.getName(), postId);
         return Response.success(PostResponse.fromPost(post));
     }
 
+    @Operation(summary = "게시글 삭제")
     @DeleteMapping("/{postId}")
     public Response<Void> delete(@PathVariable Integer postId, Authentication authentication){
         postService.delete(authentication.getName(),postId);
@@ -42,18 +46,21 @@ public class PostController {
         return Response.success();
     }
 
+    @Operation(summary = "게시글 목록 가져오기")
     @GetMapping
     public Response<Page<PostResponse>> list(Pageable pageable, Authentication authentication){
 
         return Response.success(postService.list(pageable).map(PostResponse::fromPost));
     }
 
+    @Operation(summary = "사용자 본인이 작성한 게시글 가져오기")
     @GetMapping("/my")
     public Response<Page<PostResponse>> my(Pageable pageable, Authentication authentication){
 
         return Response.success(postService.my(authentication.getName(), pageable).map(PostResponse::fromPost));
     }
 
+    @Operation(summary = "게시글 좋아요 하기")
     @PostMapping("/{postId}/likes")
     public Response<Void> like(@PathVariable Integer postId, Authentication authentication){
 
@@ -61,17 +68,19 @@ public class PostController {
         return Response.success();
     }
 
+    @Operation(summary = "게시글 좋아요 숫자 세기")
     @GetMapping("/{postId}/likes")
     public Response<Long> likeCount(@PathVariable Integer postId){
         return Response.success(postService.likeCount(postId));
     }
 
+    @Operation(summary = "댓글 작성하기")
     @PostMapping("/{postId}/comments")
     public Response<Void> comment(@PathVariable Integer postId, @RequestBody PostCommentRequest request, Authentication authentication){
         postService.comment(postId, authentication.getName(),request.getComment());
         return Response.success();
     }
-
+    @Operation(summary = "댓글 가져오기")
     @GetMapping("/{postId}/comments")
     public Response<Page<CommentResponse>> comment(@PathVariable Integer postId, Pageable pageable, Authentication authentication){
         return Response.success(postService.getComments(postId, pageable).map(CommentResponse::fromComment));
